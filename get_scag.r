d4ee81aade2f279e8dd170609a1a62fba8be7bcc
Josua Krause <josua.krause@gmail.com>
2015-11-17 18:05:42 -0800

    no diagnostic message when loading library

--- get_scag.r
+++ get_scag.r
@@ -1,4 +1,9 @@
-require(scagnostics)
+# R by default creates a warning when loading a library (even when successful)
+# we suppress this message here -- in the case the loading fails we try to
+# reload but this time with warnings enabled to get a proper error message
+if(!suppressMessages(require(scagnostics))) {
+  require(scagnostics)
+}
 
 scags <- function(x=(1:10), y=(1:10)) {
   return(scagnostics(x, y))
